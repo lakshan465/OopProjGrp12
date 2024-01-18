8602a41b0b2706385e8453bf06a51e8e50ee0e47
Lakshan <122162705+lakshan465@users.noreply.github.com>
2024-01-18 14:58:52 +0530

    First commit-lakshan

---
+++ src/javafxapplication/FXMLDocumentController.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package javafxapplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author lakshan
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Button closeBtn;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private AnchorPane mainForm;
+
+    @FXML
+    private PasswordField pwdTxt;
+
+    @FXML
+    private TextField unameTxt;
+    
+    public void close(){
+        System.exit(0);
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
